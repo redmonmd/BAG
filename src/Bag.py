44c226c1f0258a8bf6429701d96ebde33664a2ed
David Redmon <redmonmd@gmail.com>
2021-11-02 10:15:58 -0400

    add Discriminator

--- src/Bag.py
+++ src/Bag.py
@@ -57,3 +57,28 @@ class Generator(nn.Module) :
 	def forward(self, x) : 
 		return self.main(x).view(-1,1,28,28)
 
+class Discriminator(nn.Module) : 
+	def __init__(self): 
+		super(Discriminator, self).__init__()
+		self.n_input = 784
+		self.main = nn.Sequential (
+			nn.Linear(self.n_input, 1024),
+			nn.LeakyReLU(0.2),
+			nn.Dropout(0.3),
+
+			nn.Linear(1024, 512),
+			nn.LeakyReLU(0.2),
+			nn.Dropout(0.3),
+
+			nn.Linear(512, 256),
+			nn.LeakyReLU(0.2),
+			nn.Dropout(0.3),
+
+			nn.Linear(256, 1),
+			nn.Sigmoid(),
+
+		)
+	def forward(self, x) : 
+		x = x.view(-1, 784)
+		return self.main(x)
+
