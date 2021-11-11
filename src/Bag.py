8215baea7d3ded27e729adb7560b9e6bd1a938e2
David Redmon <redmonmd@gmail.com>
2021-11-11 16:18:38 -0500

    testing.. will rm swap

--- src/Bag.py
+++ src/Bag.py
@@ -38,9 +38,9 @@ train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 
 # Generator NN
 class Generator(nn.Module): 
-	def __init__(self, noise):
-		self.noise = noise
-		self.main = nn.Sequential(
+	def __init__(self, noise):	
+    self.noise = noise
+    self.main = nn.Sequential(
 			nn.Linear(self.noise, 256),
 			nn.LeakyReLU(0.2),
 			
@@ -53,7 +53,6 @@ class Generator(nn.Module):
 			nn.Linear(1024, 784),
 			nn.Tanh()
 		)
-
 	def forward(self, x) : 
 		return self.main(x).view(-1,1,28,28)
 
