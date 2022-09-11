3a5a51525cd629908ce7ad88889cbea1800f80ef
David Redmon <redmonmd@gmail.com>
2022-09-11 12:03:57 -0400

    fixed?

--- src/Bag.py
+++ src/Bag.py
@@ -38,9 +38,8 @@ train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 
 # Generator NN
 class Generator(nn.Module): 
-    def __init__(self, noise):	
-    	super(Generator(noise).__init__())
-		self.main = nn.Sequential(
+    super(Generator(noise).__init__())
+    def __init__(self, noise): self.main = nn.Sequential(
  		    nn.Linear(noise, 256),
 		    nn.LeakyReLU(0.2),
 		    nn.Linear(256, 512),
@@ -50,7 +49,7 @@ class Generator(nn.Module):
 		    nn.Linear(1024, 784),
 		    nn.Tanh(),
 		)
-    def forward(self, x): 
+def forward(self, x): 
 		return self.main(x).view(-1,1,28,28)
 
 class Discriminator(nn.Module) : 
